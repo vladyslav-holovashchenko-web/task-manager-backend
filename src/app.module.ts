e181f60b5b76ee2d2305f46b92df2e7e49201674
Vladyslav Holovashchenko <vladyslav.h.dev@gmail.com>
2024-11-20 16:48:08 +0200

    Initial app and add DB

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './config/database.config';
+import { DataSource } from 'typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+  ],
+  providers: [],
+  controllers: [],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
