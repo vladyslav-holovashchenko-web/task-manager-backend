2bba251357331a7042174a9cf2f16138d95c152b
Vladyslav Holovashchenko <vladyslav.h.dev@gmail.com>
2024-12-01 15:41:30 +0200

    added username feald

--- src/users/dto/register-user.dto.ts
+++ src/users/dto/register-user.dto.ts
@@ -1,10 +1,14 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
-  @IsEmail()
+  @IsNotEmpty()
+  @MinLength(3, { message: 'Username must be at least 3 characters long' })
+  username: string;
+
+  @IsEmail({}, { message: 'Invalid email address' })
   email: string;
 
   @IsNotEmpty()
-  @MinLength(8)
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
 }
